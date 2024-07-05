10f7182b20b943da7c796839eef1c34dc998eec6
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-07-05 22:41:46 +0600

    Adding various difficulty (a nice mining process)

--- catcoin/index.js
+++ catcoin/index.js
@@ -15,17 +15,19 @@ class Block {
             this.nonce++; 
             this.hash = this.calculateHash();
         }
+        console.log("Mining Done!: " + this.hash)
+        console.log("Loop: " + this.nonce + 1 + " times")
     }
 
     calculateHash() {
-        return sha256(this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+        return sha256(this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
     }
-
 }
 
 class BLockchain {
     constructor() {
         this.chain = [this.generateGenesisBlock()];
+        this.difficulty = 5;
     }
     generateGenesisBlock (){
         return new Block("5-Jul-2024", "GENESIS", "0000");
@@ -35,7 +37,9 @@ class BLockchain {
     }
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+        // Instructor said to add mine instead of hash 👆
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock);
     }
     isBlockChainValidate(){
@@ -52,13 +56,16 @@ class BLockchain {
 }
 
 const blockChain = new BLockchain();
-const block1 = new Block("6-Jul-2024", { cash: 100 });
-const block2 = new Block("7-Jul-2024", { cash: 200 });
 
+const block1 = new Block("6-Jul-2024", { cash: 100 });
 blockChain.addBlock(block1)
+
+const block2 = new Block("7-Jul-2024", { cash: 200 });
 blockChain.addBlock(block2)
 
-console.log(blockChain.isBlockChainValidate());
+// console.log(blockChain.isBlockChainValidate());
 
-blockChain.chain[1].data = "Hacked";
-console.log(blockChain.isBlockChainValidate());
+// blockChain.chain[1].data = "Hacked";
+// console.log(blockChain.isBlockChainValidate());
+
+console.log(blockChain);
